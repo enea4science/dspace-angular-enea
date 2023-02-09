47841adffed63fa50ebc1839d77c6fdddf7e2ce4
Giuseppe Digilio <giuseppe.digilio@4science.it>
2023-02-09 15:23:22 +0100

    Fix nested subscription

--- src/app/item-page/edit-item-page/item-status/item-status.component.ts
+++ src/app/item-page/edit-item-page/item-status/item-status.component.ts
@@ -83,120 +83,121 @@ export class ItemStatusComponent implements OnInit {
     this.itemRD$ = this.route.parent.data.pipe(map((data) => data.dso));
     this.itemRD$.pipe(
       first(),
-      map((data: RemoteData<Item>) => data.payload)
-    ).subscribe((item: Item) => {
-      this.statusData = Object.assign({
-        id: item.id,
-        handle: item.handle,
-        lastModified: item.lastModified
-      });
-      this.statusDataKeys = Object.keys(this.statusData);
+      map((data: RemoteData<Item>) => data.payload),
+      switchMap((item: Item) => {
+        this.statusData = Object.assign({
+          id: item.id,
+          handle: item.handle,
+          lastModified: item.lastModified
+        });
+        this.statusDataKeys = Object.keys(this.statusData);
 
-      // Observable for item identifiers (retrieved from embedded link)
-      this.identifiers$ = this.identifierDataService.getIdentifierDataFor(item).pipe(
-        map((identifierRD) => {
-          if (identifierRD.statusCode !== 401 && hasValue(identifierRD.payload)) {
-            return identifierRD.payload.identifiers;
-          } else {
-            return null;
-          }
-        }),
-      );
-
-      // Observable for configuration determining whether the Register DOI feature is enabled
-      let registerConfigEnabled$: Observable<boolean> = this.configurationService.findByPropertyName('identifiers.item-status.register-doi').pipe(
-        getFirstSucceededRemoteData(),
-        getRemoteDataPayload(),
-        map((enabled: ConfigurationProperty) => {
-          if (enabled !== undefined && enabled.values) {
-            return true;
-          }
-          return false;
-        })
-      );
-
-      /*
-      Construct a base list of operations.
-        The key is used to build messages
-          i18n example: 'item.edit.tabs.status.buttons.<key>.label'
-        The value is supposed to be a href for the button
-      */
-      const operations: ItemOperation[] = [];
-      operations.push(new ItemOperation('authorizations', this.getCurrentUrl(item) + '/authorizations', FeatureID.CanManagePolicies, true));
-      operations.push(new ItemOperation('mappedCollections', this.getCurrentUrl(item) + '/mapper', FeatureID.CanManageMappings, true));
-      if (item.isWithdrawn) {
-        operations.push(new ItemOperation('reinstate', this.getCurrentUrl(item) + '/reinstate', FeatureID.ReinstateItem, true));
-      } else {
-        operations.push(new ItemOperation('withdraw', this.getCurrentUrl(item) + '/withdraw', FeatureID.WithdrawItem, true));
-      }
-      if (item.isDiscoverable) {
-        operations.push(new ItemOperation('private', this.getCurrentUrl(item) + '/private', FeatureID.CanMakePrivate, true));
-      } else {
-        operations.push(new ItemOperation('public', this.getCurrentUrl(item) + '/public', FeatureID.CanMakePrivate, true));
-      }
-      operations.push(new ItemOperation('delete', this.getCurrentUrl(item) + '/delete', FeatureID.CanDelete, true));
-      operations.push(new ItemOperation('move', this.getCurrentUrl(item) + '/move', FeatureID.CanMove, true));
-      this.operations$.next(operations);
-
-      /*
-       When the identifier data stream changes, determine whether the register DOI button should be shown or not.
-       This is based on whether the DOI is in the right state (minted or pending, not already queued for registration
-       or registered) and whether the configuration property identifiers.item-status.register-doi is true
-       */
-      this.identifierDataService.getIdentifierDataFor(item).pipe(
-        getFirstSucceededRemoteData(),
-        getRemoteDataPayload(),
-        mergeMap((data: IdentifierData) => {
-          let identifiers = data.identifiers;
-          let no_doi = true;
-          let pending = false;
-          if (identifiers !== undefined && identifiers !== null) {
-            identifiers.forEach((identifier: Identifier) => {
-              if (hasValue(identifier) && identifier.identifierType === 'doi') {
-                // The item has some kind of DOI
-                no_doi = false;
-                if (identifier.identifierStatus === 'PENDING' || identifier.identifierStatus === 'MINTED'
-                  || identifier.identifierStatus == null) {
-                  // The item's DOI is pending, minted or null.
-                  // It isn't registered, reserved, queued for registration or reservation or update, deleted
-                  // or queued for deletion.
-                  pending = true;
-                }
-              }
-            });
-          }
-          // If there is no DOI, or a pending/minted/null DOI, and the config is enabled, return true
-          return registerConfigEnabled$.pipe(
-            map((enabled: boolean) => {
-              return enabled && (pending || no_doi);
+        // Observable for item identifiers (retrieved from embedded link)
+        this.identifiers$ = this.identifierDataService.getIdentifierDataFor(item).pipe(
+          map((identifierRD) => {
+            if (identifierRD.statusCode !== 401 && hasValue(identifierRD.payload)) {
+              return identifierRD.payload.identifiers;
+            } else {
+              return null;
             }
-          ));
-        }),
-        // Switch map pushes the register DOI operation onto a copy of the base array then returns to the pipe
-        switchMap((showDoi: boolean) => {
-          let ops = [...operations];
-          if (showDoi) {
-            ops.push(new ItemOperation('register-doi', this.getCurrentUrl(item) + '/register-doi', FeatureID.CanRegisterDOI, true));
-          }
-          return ops;
-        }),
-        // Merge map checks and transforms each operation in the array based on whether it is authorized or not (disabled)
-        mergeMap((op: ItemOperation) => {
-          if (hasValue(op.featureID)) {
-            return this.authorizationService.isAuthorized(op.featureID, item.self).pipe(
-              distinctUntilChanged(),
-              map((authorized) => new ItemOperation(op.operationKey, op.operationUrl, op.featureID, !authorized, authorized))
-            );
-          } else {
-            return [op];
-          }
-        }),
-        // Wait for all operations to be emitted and return as an array
-        toArray(),
-      ).subscribe((data) => {
-        // Update the operations$ subject that draws the administrative buttons on the status page
-        this.operations$.next(data);
-      });
+          }),
+        );
+
+        // Observable for configuration determining whether the Register DOI feature is enabled
+        let registerConfigEnabled$: Observable<boolean> = this.configurationService.findByPropertyName('identifiers.item-status.register-doi').pipe(
+          getFirstSucceededRemoteData(),
+          getRemoteDataPayload(),
+          map((enabled: ConfigurationProperty) => {
+            if (enabled !== undefined && enabled.values) {
+              return true;
+            }
+            return false;
+          })
+        );
+
+        /*
+        Construct a base list of operations.
+          The key is used to build messages
+            i18n example: 'item.edit.tabs.status.buttons.<key>.label'
+          The value is supposed to be a href for the button
+        */
+        const operations: ItemOperation[] = [];
+        operations.push(new ItemOperation('authorizations', this.getCurrentUrl(item) + '/authorizations', FeatureID.CanManagePolicies, true));
+        operations.push(new ItemOperation('mappedCollections', this.getCurrentUrl(item) + '/mapper', FeatureID.CanManageMappings, true));
+        if (item.isWithdrawn) {
+          operations.push(new ItemOperation('reinstate', this.getCurrentUrl(item) + '/reinstate', FeatureID.ReinstateItem, true));
+        } else {
+          operations.push(new ItemOperation('withdraw', this.getCurrentUrl(item) + '/withdraw', FeatureID.WithdrawItem, true));
+        }
+        if (item.isDiscoverable) {
+          operations.push(new ItemOperation('private', this.getCurrentUrl(item) + '/private', FeatureID.CanMakePrivate, true));
+        } else {
+          operations.push(new ItemOperation('public', this.getCurrentUrl(item) + '/public', FeatureID.CanMakePrivate, true));
+        }
+        operations.push(new ItemOperation('delete', this.getCurrentUrl(item) + '/delete', FeatureID.CanDelete, true));
+        operations.push(new ItemOperation('move', this.getCurrentUrl(item) + '/move', FeatureID.CanMove, true));
+        this.operations$.next(operations);
+
+        /*
+         When the identifier data stream changes, determine whether the register DOI button should be shown or not.
+         This is based on whether the DOI is in the right state (minted or pending, not already queued for registration
+         or registered) and whether the configuration property identifiers.item-status.register-doi is true
+         */
+        return this.identifierDataService.getIdentifierDataFor(item).pipe(
+          getFirstSucceededRemoteData(),
+          getRemoteDataPayload(),
+          mergeMap((data: IdentifierData) => {
+            let identifiers = data.identifiers;
+            let no_doi = true;
+            let pending = false;
+            if (identifiers !== undefined && identifiers !== null) {
+              identifiers.forEach((identifier: Identifier) => {
+                if (hasValue(identifier) && identifier.identifierType === 'doi') {
+                  // The item has some kind of DOI
+                  no_doi = false;
+                  if (identifier.identifierStatus === 'PENDING' || identifier.identifierStatus === 'MINTED'
+                    || identifier.identifierStatus == null) {
+                    // The item's DOI is pending, minted or null.
+                    // It isn't registered, reserved, queued for registration or reservation or update, deleted
+                    // or queued for deletion.
+                    pending = true;
+                  }
+                }
+              });
+            }
+            // If there is no DOI, or a pending/minted/null DOI, and the config is enabled, return true
+            return registerConfigEnabled$.pipe(
+              map((enabled: boolean) => {
+                  return enabled && (pending || no_doi);
+                }
+              ));
+          }),
+          // Switch map pushes the register DOI operation onto a copy of the base array then returns to the pipe
+          switchMap((showDoi: boolean) => {
+            let ops = [...operations];
+            if (showDoi) {
+              ops.push(new ItemOperation('register-doi', this.getCurrentUrl(item) + '/register-doi', FeatureID.CanRegisterDOI, true));
+            }
+            return ops;
+          }),
+          // Merge map checks and transforms each operation in the array based on whether it is authorized or not (disabled)
+          mergeMap((op: ItemOperation) => {
+            if (hasValue(op.featureID)) {
+              return this.authorizationService.isAuthorized(op.featureID, item.self).pipe(
+                distinctUntilChanged(),
+                map((authorized) => new ItemOperation(op.operationKey, op.operationUrl, op.featureID, !authorized, authorized))
+              );
+            } else {
+              return [op];
+            }
+          }),
+          // Wait for all operations to be emitted and return as an array
+          toArray(),
+        )
+      })
+    ).subscribe((data) => {
+      // Update the operations$ subject that draws the administrative buttons on the status page
+      this.operations$.next(data);
     });
 
     this.itemPageRoute$ = this.itemRD$.pipe(
